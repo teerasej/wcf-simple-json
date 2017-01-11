d598aa96ac9b9c5ec181eadef9d369ddb02a5b8b
Teerasej Jiraphatchandej <teerasej@gmail.com>
2017-01-11 23:54:32 +0700

    Add project files.

---
+++ ContactService/AccountContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace ContactService
+{
+    [DataContract]
+    public class AccountContract
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public string Tel { get; set; }
+    }
+}
